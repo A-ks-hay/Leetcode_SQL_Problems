09f017e79e4031db7a17996595363d888004149d
Akshay kumar <kumaraashu29508@gmail.com>
2025-12-29 16:16:07 +0530

    Time: 1743 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+select distinct a.num as ConsecutiveNums from logs a where a.num=(select num from logs where id=a.id+1)
+and a.num=(select num from Logs where id=a.id+2)
